8d164f3d6ac392d66a8bfea649cd8844861ba459
Galymzhan <galymzhan.aitkaliyev@gmail.com>
2025-01-27 16:10:18 +0500

    created StoreController and StoreRequest -Added route
    route without namespace, just prefix

---
+++ app/Http/Controllers/Person/StoreController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Person;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class StoreController extends Controller
+{
+    public function __invoke() {
+        $persons = [];
+
+        return $persons;
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,4 @@ Route::get('/', function () {
 // Auth::routes();
 Route::get("/home", [App\Http\Controllers\HomeController::class, 'index']) -> name('home');
 
-Route::get("/persons", PersonController::class); 
+// Route::get("/persons", PersonController::class); 
