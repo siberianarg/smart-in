17c99fddfc59fef5456f0830b1d43f3b88ed3d83
Galymzhan <galymzhan.aitkaliyev@gmail.com>
2025-01-28 17:00:37 +0500

    fixed postComponent

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -2,24 +2,6 @@
     <div>
         <createComponent> </createComponent>
         <indexComponent></indexComponent>
-        <!-- <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Age</th>
-                    <th scope="col">Jobe</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="person in persons">
-                    <th scope="row">{{ person.id }}</th>
-                    <td>{{ person.name }}</td>
-                    <td>{{ person.age }}</td>
-                    <td>{{ person.job }}</td>
-                </tr>
-            </tbody>
-        </table> -->
     </div>
 </template>
 
@@ -35,28 +17,10 @@ export default {
         indexComponent,
     },
     data() {
-        return {
-            persons: null
-        };
+        return {};
     },
-
-    mounted() {
-        // this.getPersons()
-    },
-
-    methods: {
-        // getPersons() {
-        //     axios.get('/persons')
-        //     .then( res =>  // data from backend
-        //     {
-        //         this.persons = res.data
-        //     })
-        //     .catch( error => {
-        //     })
-        //     .finally({
-        //     })
-        // }
-    },
+    mounted() {},
+    methods: {},
     computed: {},
     watch: {},
 };
