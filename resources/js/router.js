58ae3fbd4f8bed602cbe26408e293bbd10a602d9
Galymzhan <galymzhan.aitkaliyev@gmail.com>
2025-02-03 09:41:49 +0500

    поправка кода после ревью

--- resources/js/router.js
+++ resources/js/router.js
@@ -3,22 +3,22 @@ import { createRouter, createWebHistory } from "vue-router";
 const routes = [
     {
         path: "/tasks",
-        component: () => import("./components/Task/index.vue"),
+        component: () => import("./components/Task/indexComponent.vue"),
         name: "task.index",
     },
     {
         path: "/tasks/add",
-        component: () => import("./components/Task/add.vue"),
+        component: () => import("./components/Task/addComponent.vue"),
         name: "task.add",
     },
     {
         path: "/tasks/edit/:id",
-        component: () => import("./components/Task/edit.vue"),
+        component: () => import("./components/Task/editComponent.vue"),
         name: "task.edit",
     },
     {
         path: "/tasks/:id", 
-        component: () => import("./components/Task/show"),
+        component: () => import("./components/Task/showComponent"),
         name: "task.show",
     }
     
