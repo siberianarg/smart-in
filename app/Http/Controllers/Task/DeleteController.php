a9da6c6b871d5afa78bf22a0243864d2b7299e73
Galymzhan <galymzhan.aitkaliyev@gmail.com>
2025-02-05 09:26:47 +0500

    Логируем ошибки в DeleteController
    Задача удаляется в МойСклад, но НЕ удаляется в MySQL

--- app/Http/Controllers/Task/DeleteController.php
+++ app/Http/Controllers/Task/DeleteController.php
@@ -5,11 +5,11 @@ namespace App\Http\Controllers\Task;
 use App\Http\Controllers\Controller;
 use App\Models\Task;
 use App\Client\MoySkladClient;
-use App\Http\Controllers\HomeController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
-class DeleteController extends Controller {
-
+class DeleteController extends Controller
+{
     private MoySkladClient $msClient;
 
     public function __construct(MoySkladClient $msClient)
@@ -19,20 +19,29 @@ class DeleteController extends Controller {
 
     public function __invoke($id)
     {
+        // Ищем задачу в локальной БД
         $task = Task::find($id);
 
         if (!$task) {
+            Log::warning("Task not found in local DB: ID $id");
             return response()->json(['error' => 'Task not found'], 404);
         }
 
         $ms_uuid = $task->ms_uuid;
+
+        // Удаляем задачу из МойСклад
         $deleted = $this->msClient->deleteTask($ms_uuid);
 
-        if ($deleted) {
-            $task->delete();
-            return response()->json(null, 204);
+        if (!$deleted) {
+            Log::error("Failed to delete task in MoySklad: MS_UUID $ms_uuid");
+            return response()->json(['error' => 'Failed to delete task in MoySklad'], 500);
         }
 
-        return response()->json(['error' => 'Failed to delete task in MoySklad'], 500);
+        // Удаляем задачу из локальной БД
+        $task->delete();
+
+        Log::info("Task successfully deleted: Local ID $id, MS_UUID $ms_uuid");
+
+        return response()->json(null, 204);
     }
 }
